a689835277f4c7e04e1988fef6ffe80c12f6b87d
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2019-02-18 13:28:33 -0500

    Fixed the initialization.

--- app/src/main/java/ca/centennialcollege/rodrigodasilva_comp304_002_assign03/LineDrawerActivity.java
+++ app/src/main/java/ca/centennialcollege/rodrigodasilva_comp304_002_assign03/LineDrawerActivity.java
@@ -82,7 +82,7 @@ public class LineDrawerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_drawer);
 
-        //initialize();
+        initialize();
     }
 
     /**
